9c4b53e3457efd1b9b1021e29a29ff8db856e5ba
charan v <durgasudheerv@gmail.com>
2025-12-25 11:59:37 +0530

    bfb

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "check_uppercase.h"
+
+int main(void) {
+    char str[MAX];
+    scan_data(char *str) 
+    check_is_uppercase(const char *str);
+    printf("Uppercase: %s\n", str);
+    return 0;
+}
